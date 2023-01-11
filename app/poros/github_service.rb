dc0a45ff83ef2ec589ed2b107ef9f39d932d1815
Brandon Gray <brandonjoe88@hotmail.com>
2023-01-11 11:50:50 -0700

    fix: remove attr_readers

--- app/poros/github_service.rb
+++ app/poros/github_service.rb
@@ -1,10 +1,7 @@
-# require './app/poros/github_runner.rb'
 require 'httparty'
 require 'json'
 require 'pry'
-# # require_relative './commit_search.rb'
 require_relative './commit.rb'
-# require_relative './pull_request_search.rb'
 require_relative './pull_request.rb'
 # require_relative './repo_name_search.rb'
 # require_relative './repo_name.rb'
@@ -12,9 +9,6 @@ require_relative './pull_request.rb'
 # require_relative './user_name.rb'
 
 class GithubService
-
-  attr_reader :pr_count,
-              :tc_count
 
   def self.github_api_pr
     PullRequest.new.parsed.count
