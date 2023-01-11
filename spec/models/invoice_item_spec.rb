fe37b5a2e4bcaa4c7227f34b477bc9f9f68d61db
Brady Rohrig <brady.rohrig@gmail.com>
2023-01-11 10:32:19 -0800

    test: wrote model test for invoice_item revenue method

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe InvoiceItem, type: :model do
     it {should have_one(:customer).through(:invoice)}
     it {should have_many(:transactions).through(:invoice)}
   end
+
+  it 'has a method to find the revenue for instances of itself' do
+    InvoiceItem.delete_all
+    @merchant1 = Merchant.create!(name: "Schroeder-Jerde")
+    @item1 = Item.create!(name: "Item 1", unit_price: 75107, description: "it's a thing that does stuff", merchant_id: @merchant1.id)
+    @customer1 = Customer.create!(first_name: "Joey", last_name: "Ondricka")
+    @invoice1 = Invoice.create!(customer_id: @customer1.id, status: "completed")
+    @invoice_item1 = InvoiceItem.create!(item_id: @item1.id, invoice_id: @invoice1.id, quantity: 23, unit_price: 2, status: "shipped")
+
+    expect(InvoiceItem.revenue).to eq(0.46)
+  end
 end
